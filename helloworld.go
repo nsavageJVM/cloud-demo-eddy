608db01077320e8001578f5f93974427366d04cb
nsavagejvm <nscode1@gmail.com>
2016-09-18 01:21:13 +0300

    cdm -src-update

--- helloworld.go
+++ helloworld.go
@@ -3,16 +3,11 @@ package helloworld
 import (
 	"fmt"
 	"net/http"
-	"github.com/gorilla/sessions"
 )
-
-var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 func init() {
 	http.HandleFunc("/", handle)
 }
-
-
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<html><body>Eddys Go Cloud Platform Service </body></html>")
